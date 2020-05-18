d5a8a0bbfff2140044fc166c0193f9d0e4916c76
Gourang Nagar <65488240+krypto6402@users.noreply.github.com>
2020-05-19 00:39:35 +0530

    Update and rename large in 3.c to big&small_in_3.c

--- big&small_in_3.c
+++ big&small_in_3.c
@@ -35,3 +35,24 @@ int main()
  
     return 0;
 }
+
+// SMALL
+/*
+#include <stdio.h> 
+int main() 
+{ 
+    int a,b,c, small; 
+    printf("Enter first number :");
+    scanf("%d",&a);
+    printf("Enter second number :");
+    scanf("%d",&b);
+    printf("Enter third number :");
+    scanf("%d",&c);
+      
+    small = (a < b) ? (a < c ? a : c) : (b < c ? b : c); 
+      
+    printf("Smallest number among %d, %d and %d is %d.", a, b, c, small); 
+  
+    return 0; 
+}
+*/
