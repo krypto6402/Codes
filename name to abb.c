da296e3e1e54dedbf21dcb3df44f8fd20a7c345a
Gourang Nagar <65488240+krypto6402@users.noreply.github.com>
2020-05-29 00:15:50 +0530

    Update name to abb.c

--- name to abb.c
+++ name to abb.c
@@ -25,3 +25,25 @@ int main()
   printf("%c",b[i]);
   return 0;
 }
+
+
+
+
+
+
+/*
+
+SECOND METHOD
+
+#include<stdio.h>
+int main()
+{
+     char fname[20], mname[20], lname[20];
+     printf("Enter The First Name Middle Name & Last Name \n");
+     scanf("%s %s %s", fname, mname, lname);
+     printf("Abbreviated name: ");
+     printf("%c. %c. %s\n", fname[0], mname[0], lname);
+     return 0;
+}
+
+*/
