a455265dfd962ba34523287b39cd76888a0e8d8d
Gourang Nagar <65488240+krypto6402@users.noreply.github.com>
2020-06-21 13:37:25 +0530

    Update capitalize first letter.py

--- capitalize first letter.py
+++ capitalize first letter.py
@@ -2,3 +2,29 @@ s=input()
 a=s.split()
 for i in range(len(a)):
     print(a[i].capitalize(),end=" ")
+    
+    
+def solve(s):
+o = []
+s = list(map(str,s.split(' ')))
+for i in s:
+o.append(i.capitalize())
+return ' '.join(o)
+
+
+
+
+
+
+print(" ".join(word.capitalize() for word in input().split(' ')))
+
+
+s = input()
+for x in s[:].split():
+    s = s.replace(x, x.capitalize())
+print(s)
+
+
+
+
+
